d5514a33d9ce5c8ddab00c6a2764b49f7e365a74
Abhijeet Kulshreshtha <abhijeet.abap@gmail.com>
2022-06-11 13:05:54 +0100

    Type Guards

--- src/app.ts
+++ src/app.ts
@@ -21,3 +21,57 @@ console.log(e1);
 type Combinable = string | number;
 type Numeric = number | boolean;
 type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInfo(emp: UnknownEmployee) {
+    console.log('Name: ' + emp.name);
+    if ('privileges' in emp) {
+        console.log('Privileges: ' + emp.privileges);
+    }
+
+    if ('startDate' in emp) {
+        console.log('Start Date: ' + emp.startDate);
+    }    
+}
+
+printEmployeeInfo(e1);
+printEmployeeInfo({name: 'Ankur', startDate: new Date()});
+
+class Car {
+    drive() {
+        console.log('driving...')
+    }
+}
+
+class Truck{
+    drive() {
+        console.log('driving Truck...')
+    }    
+
+    loadCargo(amount: number) {
+        console.log('Loading Cargo...' + amount);
+    }        
+}
+
+type Vehicle = Car | Truck;
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(100);
+    }
+}
+
+useVehicle(v1);
+useVehicle(v2);
+
