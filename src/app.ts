bffbe88e455da392b70a4f0436ae29a8a977625a
Abhijeet Kulshreshtha <abhijeet.abap@gmail.com>
2022-06-11 13:37:48 +0100

    Discriminated Unions

--- src/app.ts
+++ src/app.ts
@@ -75,3 +75,28 @@ function useVehicle(vehicle: Vehicle) {
 useVehicle(v1);
 useVehicle(v2);
 
+interface Bird {
+    type: 'Bird';
+    flyingSpeed: number;
+}
+
+interface Horse {
+    type: 'Horse';
+    runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+function moveAnimal(animal: Animal) {
+    let speed;
+    switch (animal.type) {
+        case 'Bird':
+            speed = animal.flyingSpeed;
+            break;
+        case 'Horse':
+            speed = animal.runningSpeed;
+            break;
+    }
+    console.log(animal.type + ' is moving at Speed: ' + speed);
+}
+
+moveAnimal({type: 'Bird', flyingSpeed: 30});
